6feb68877b67e05913d240fd99f8e5eaba96d86c
Rajpatel1098 <87034183+Rajpatel1098@users.noreply.github.com>
2021-10-11 08:46:37 +0530

    Add files via upload

--- Conditional Statements.c
+++ Conditional Statements.c
@@ -1,17 +1,40 @@
 #include<stdio.h>
-main()
+ 
+int main() 
 {
-	printf("Select an number:\n1.0\n2.1");
-	int choice=0;
-	scanf("%d",&choice);
-	
-	switch(choice)
-	{
-		case 1:
-			printf("You select 0");
-		case 2:
-			printf("You select1");
-		default:
-			printf("Invalid choice");
-	}
+    float marks;
+    char grade;
+ 
+    printf("Enter marks: ");
+    scanf("%f", &marks);
+    
+    if(marks >= 90)
+    {
+        grade = 'A';
+    }
+    else if(marks >= 80 && marks < 90)
+    {
+        grade = 'B';
+    }
+    else if(marks >= 70 && marks < 80)
+    {
+        grade = 'C';
+    }
+    else if(marks >= 60 && marks < 70)
+    {
+        grade = 'D';
+    }
+    else if(marks >= 50 && marks < 60)
+    {
+        grade = 'E';
+    }
+    else 
+    {
+        grade = 'F';
+    }
+                    
+    printf("Your grade is %c", grade);
+    
+    return 0;
 }
+
